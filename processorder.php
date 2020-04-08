77e83fa99a671ecfb8de92e02a4ee962a3037930
raspi-home <piscon@qq.com>
2020-04-09 00:28:11 +0900

    php first day

---
+++ processorder.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bob's Auto Parts - Order Results</title>
+</head>
+<body>
+    <h1>Bob's Auto Parts</h1>
+    <h2>Order Results</h2>
+    <?php
+        echo '<p>Order processed.</p>';
+    ?>
+</body>
+</html>
